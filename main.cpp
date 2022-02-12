2fa9d5741f7eb1d38d182d419b399e13328b235e
Arnaud Blondel <ablondel@fu-r7-p8.s19.be>
2022-02-12 17:35:30 +0100

    go read the man

--- main.cpp
+++ main.cpp
@@ -6,7 +6,45 @@
 /*   By: ablondel <ablondel@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/12 15:41:50 by ablondel          #+#    #+#             */
-/*   Updated: 2022/02/12 16:15:30 by ablondel         ###   ########.fr       */
+/*   Updated: 2022/02/12 17:30:58 by ablondel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+vector class
+https://www.geeksforgeeks.org/program-to-create-custom-vector-class-in-c/
+
+	vector()
+	~vector
+	operator=
+	begin
+	end
+	rbegin
+	rend
+	size
+	max_size
+	empty
+	resize
+	capacity
+	reserve
+	front
+	back
+	operator[]
+	at
+	assign
+	insert
+	erase
+	push_back
+	pop_back
+	clear
+	swap
+	get_allocator
+
+/////////////////////////////
+
+iterator
+https://anderberg.me/2016/07/04/c-custom-iterators/
+https://stackoverflow.com/questions/21275190/c-custom-collection-reverse-iterator-with-similar-behaviour-to-stdvector-imp
+
+
+*/
